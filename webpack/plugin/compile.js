9b4acccf2b9eb6f61f1a60833e6cfb69b9cedd8a
kevin0z0 <644720213@qq.com>
2022-08-27 23:15:19 +0800

    dot auto move

--- webpack/plugin/compile.js
+++ webpack/plugin/compile.js
@@ -1,17 +1,17 @@
-const {NormalModule, Compilation} = require('webpack')
+const { NormalModule, Compilation } = require('webpack')
 const PLUGIN_NAME = 'CompilePlugin'
 
 class CompilePlugin {
     constructor() {
 
     }
-  
+
     apply(compiler) {
         compiler.hooks.compilation.tap(PLUGIN_NAME, (compilation) => {
             const modifiedModules = new Set()
             const tapCallback = (_, normalModule) => {
                 const userRequest = normalModule.userRequest.replace(/\\/g, '/') || '';
-                if(!/dev\/layout\/.*.js$/.test(userRequest)) return
+                if (!/dev\/layout\/.*.js$/.test(userRequest)) return
                 if (modifiedModules.has(userRequest)) return
                 normalModule.loaders = [
                     {
@@ -27,9 +27,9 @@ class CompilePlugin {
             NormalModule.getCompilationHooks(compilation).beforeLoaders.tap(
                 PLUGIN_NAME,
                 tapCallback
-              );
-          });
+            );
+        });
     }
-  }
+}
 
 module.exports = CompilePlugin
