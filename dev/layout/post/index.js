6cb2d11b2558ab5a8b2bd9cc8810b001ca3c54ca
kevin0z0 <644720213@qq.com>
2023-03-10 23:43:02 +0800

    add smooth scroll

--- dev/layout/post/index.js
+++ dev/layout/post/index.js
@@ -1,6 +1,19 @@
 import '../../utils/markdown';
 import tabs from '../../components/icon';
+import scrollIntoView from 'scroll-into-view-if-needed'
+
 
 
 
 tabs('header-small__right__list-name')
+const title = document.getElementsByClassName("post-banner__wrap-title")[0]
+const main = document.getElementsByClassName("post")[0]
+title.addEventListener('click', () =>{
+    scrollIntoView(main, { behavior: 'smooth', scrollMode: 'always', block: 'start', inline: 'start'})
+})
+
+window.addEventListener("load", () => {
+    const hash = decodeURI(location.hash).substring(1)
+    const item = document.getElementById(hash)
+    if(item) scrollIntoView(item, { behavior: 'smooth', scrollMode: 'always', block: 'start', inline: 'start'})
+})
--- scripts/utils/functions.js
+++ scripts/utils/functions.js
@@ -1,4 +1,9 @@
-hexo.extend.helper.register('fillPath', function(arg){
+function fillPath(arg){
     if(arg && !arg.startsWith('/')) return '/' + arg
     return arg
-});
+}
+hexo.extend.helper.register('fillPath', fillPath);
+hexo.extend.helper.register('banner', function(img){
+    if(!img) return fillPath(this.theme.default_banner)
+    return fillPath(img)
+})
